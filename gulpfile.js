694ed8ea298f0db0aa66c3e9eff031e26bb8ed6b
unknown <Torquato, Nathan>
2017-10-24 16:37:52 -0200

    Separate tasks and run all by default

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
 const gulp   = require('gulp'),
       uglify = require('gulp-uglify');
 
-gulp.task('default', () => {
-  // console.log('Hello Gulp');
+// Scripts Task
+// Uglifies *.js
+gulp.task('scripts', () => {
+  // console.log('test');
   gulp.src('js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'))
 })
+
+// Styles Task
+// Minifies *.css
+gulp.task('styles', () => {
+  console.log('Styles');
+})
+
+const tasks = [
+  'scripts',
+  'styles'
+]
+
+gulp.task('default', tasks);
